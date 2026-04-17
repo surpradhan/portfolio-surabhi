61153dedaa630350d369320454aa5e3570e2f208
Surabhi Pradhan <surabhi7pradhan@gmail.com>
2026-04-17 11:58:40 +0530

    basePath fix

--- site/next.config.mjs
+++ site/next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  basePath: '/portfolio-surabhi',
+  assetPrefix: '/portfolio-surabhi',
 };
 
 export default nextConfig;
